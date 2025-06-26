614ba21af052f5431206a8489d8d876df8b24882
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-06-26 14:32:26 +0500

    New Commit

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const topBar = document.getElementById('topBar');
+const exteriorButtons = document.querySelector("#exterior-buttons");
+const interiorButtons = document.querySelector("#interior-buttons")
+const exteriorImage = document.getElementById("exteriorImage");
+const interiorImage = document.getElementById("interiorImage");
 
 // Handle Top Bar Scroll
 const handleScroll = () => {
@@ -7,7 +11,26 @@ const handleScroll = () => {
     topBar.classList.toggle("hidden-bar", !atTop);
 };
 
+// Handle Color Selection
+
+const handleColorButtonClick = (e) => {
+    let button;
+
+    if(e.target.tagName === "IMG"){
+        button = e.target.closest('button');
+    }else if (e.target.tagName === "BUTTON"){
+        button = e.target;
+    }
+
+    if(button){
+        const buttons = e.currentTarget.querySelectorAll('button');
+        buttons.forEach((btn) => btn.classList.remove('button_selected'))
+        button.classList.add('button_selected')
+    }
+}
+
 
 // Event Listeners
-
 window.addEventListener('scroll', ()=>  requestAnimationFrame(handleScroll));
+exteriorButtons.addEventListener('click', handleColorButtonClick);
+interiorButtons.addEventListener('click', handleColorButtonClick);
